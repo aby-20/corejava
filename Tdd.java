05a6a80dadf5b92b500d8786c77e2fe727bfd864
aby-20 <abysyril1919@gmail.com>
2025-06-10 16:02:06 +0530

    Odd or Even

---
+++ Tdd.java
@@ -0,0 +1,14 @@
+public class Tdd{
+ public static void main(String[] args){
+System.out.print("hello world");
+int num=23;
+if(num%2==0)
+{
+System.out.print("number is even");
+}
+else{
+System.out.print("number is odd");
+}
+}
+}
+
