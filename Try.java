595ddcca35dbb6a14f3e50405945da2eb88098e4
aby-20 <abysyril1919@gmail.com>
2025-06-13 12:56:52 +0530

    TryandCatch

---
+++ Try.java
@@ -0,0 +1,10 @@
+public class Try{
+public static void main(String[] args){
+try{
+int num[]={1,2,3};
+System.out.print(num[10]);
+}catch (Exception e){
+System.out.print("Something went wrong");
+}
+}
+}
