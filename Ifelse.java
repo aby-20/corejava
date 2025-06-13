7ad31af1c0a39a16ce8d026d296ab6d19af9000b
aby-20 <abysyril1919@gmail.com>
2025-06-13 11:15:23 +0530

    Control

---
+++ Ifelse.java
@@ -0,0 +1,10 @@
+public class Ifelse{
+public static void main(String[] args){
+if (10>20){
+System.out.print("10 is greater than 20");
+}
+else{
+System.out.print("20 is greater than 10");
+}
+}
+}
