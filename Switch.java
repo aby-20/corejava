c099e90f8ab1c1867a92f6b050d152a1460b8752
aby-20 <abysyril1919@gmail.com>
2025-06-13 11:24:30 +0530

    Control

---
+++ Switch.java
@@ -0,0 +1,16 @@
+public class Switch{
+public static void main(String[] args){
+int day=6;
+
+switch(day){
+case 6:
+System.out.print("Today is Saturday");
+break;
+case 7:
+System.out.print("Today is Sunday");
+break;
+default:
+System.out.print("Weekday");
+}
+}
+}
